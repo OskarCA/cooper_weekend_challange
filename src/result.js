f05660a8db8ce2d564089345edce2816ef9e9fbf
Holger_Tidemand <tidemand.holger@gmail.com>
2018-02-03 14:11:51 +0100

    finished result_mail function

--- src/result.js
+++ src/result.js
@@ -1,64 +1,276 @@
 function Result() {}
 //Stack Overflow - https://stackoverflow.com/questions/14718561/how-to-check-if-a-number-is-between-two-values
 Number.prototype.between = function(a, b) {
-    var min = Math.min(a, b),
-        max = Math.max(a, b);
+  var min = Math.min(a, b),
+    max = Math.max(a, b);
 
-    return this >= min && this <= max;
+  return this >= min && this <= max;
 };
 
 // function reult
 
 
 Result.prototype.gender_check = function(obj) {
-    switch (true) {
-        case obj.gender === 'male':
-            return Result.prototype.result_male(obj);
-            break;
-        case obj.gender === 'female':
-            return Result.prototype.result_female(obj);
-            break;
-    }
-}
+  switch (true) {
+    case obj.gender === 'male':
+      return Result.prototype.result_male(obj);
+    case obj.gender === 'female':
+      return Result.prototype.result_female(obj);
+  }
+};
 
 Result.prototype.result_male = function(obj) {
-    switch(true) {
-        case obj.age.between(13, 14):
-            switch (true) {
-                case (obj.distance >= 2700):
-                    return 'Excellent'
-                    break;
-                case (obj.distance.between(2400, 2699)):
-                    return 'Above Average'
-                    break;
-                case (obj.distance.between(2200, 2399)):
-                    return 'Average'
-                    break;
-                case (obj.distance.between(2100, 2199)):
-                    return 'Below Average'
-                    break;
-                case (obj.distance <= 2100):
-                    return 'Poor'
-                break;
+  switch (true) {
+    case obj.age.between(13, 14):
+      switch (true) {
+        case obj.distance >= 2700:
+          return 'Excellent';
+        case obj.distance.between(2400, 2699):
+          return 'Above Average';
 
-                }
-            break;
-    }
+        case obj.distance.between(2200, 2399):
+          return 'Average';
 
-        // case obj.age.between(13, 14):
-        //     switch (true) {
-        //         case obj.distance >= 2700:
-        //             return 'Excellent'
-        //         case obj.distance.between(2400, 2699):
-        //             return 'Above Average'
-        //         case obj.distance.between(2200, 2399):
-        //             return 'Average'
-        //         case obj.distance.between(2100, 2199):
-        //             return 'Below Average'
-        //         case obj.distance <= 2100:
-        //             return 'Poor'
-        //
-        //     }
-        //     break;
-        // }
-}
+        case obj.distance.between(2100, 2199):
+          return 'Below Average';
+
+        case (obj.distance <= 2100):
+          return 'Poor';
+
+
+      }
+      break;
+
+    case obj.age.between(15, 16):
+      switch (true) {
+        case obj.distance >= 2800:
+          return 'Excellent';
+        case obj.distance.between(2500, 2799):
+          return 'Above Average';
+        case obj.distance.between(2300, 2499):
+          return 'Average';
+        case obj.distance.between(2200, 2299):
+          return 'Below Average';
+        case obj.distance <= 2200:
+          return 'Poor';
+      }
+      break;
+    case obj.age.between(17, 19):
+      switch (true) {
+        case obj.distance >= 3000:
+          return 'Excellent';
+        case obj.distance.between(2700, 2999):
+          return 'Above Average';
+        case obj.distance.between(2500, 2699):
+          return 'Average';
+        case obj.distance.between(2300, 2599):
+          return 'Below Average';
+        case obj.distance <= 2300:
+          return 'Poor';
+      }
+      break;
+
+    case obj.age.between(15, 16):
+      switch (true) {
+        case obj.distance >= 2800:
+          return 'Excellent';
+        case obj.distance.between(2500, 2799):
+          return 'Above Average';
+        case obj.distance.between(2300, 2499):
+          return 'Average';
+        case obj.distance.between(2200, 2299):
+          return 'Below Average';
+        case obj.distance <= 2200:
+          return 'Poor';
+      }
+      break;
+
+    case obj.age.between(20, 29):
+      switch (true) {
+        case obj.distance >= 2800:
+          return 'Excellent';
+        case obj.distance.between(2400, 2799):
+          return 'Above Average';
+        case obj.distance.between(2200, 2399):
+          return 'Average';
+        case obj.distance.between(1600, 2199):
+          return 'Below Average';
+        case obj.distance <= 1600:
+          return 'Poor';
+      }
+      break;
+
+    case obj.age.between(30, 39):
+      switch (true) {
+        case obj.distance >= 2700:
+          return 'Excellent';
+        case obj.distance.between(2300, 2699):
+          return 'Above Average';
+        case obj.distance.between(2000, 2299):
+          return 'Average';
+        case obj.distance.between(1500, 1999):
+          return 'Below Average';
+        case obj.distance <= 1500:
+          return 'Poor';
+      }
+      break;
+
+    case obj.age.between(40, 49):
+      switch (true) {
+        case obj.distance >= 2500:
+          return 'Excellent';
+        case obj.distance.between(2100, 2499):
+          return 'Above Average';
+        case obj.distance.between(1700, 2099):
+          return 'Average';
+        case obj.distance.between(1400, 1699):
+          return 'Below Average';
+        case obj.distance <= 1400:
+          return 'Poor';
+      }
+      break;
+
+    case (obj.age >= 50):
+      switch (true) {
+        case obj.distance >= 2400:
+          return 'Excellent';
+        case obj.distance.between(2000, 2399):
+          return 'Above Average';
+        case obj.distance.between(1600, 1999):
+          return 'Average';
+        case obj.distance.between(1300, 1599):
+          return 'Below Average';
+        case obj.distance <= 1300:
+          return 'Poor';
+      }
+      break;
+  }
+};
+
+Result.prototype.result_male = function(obj) {
+  switch (true) {
+    case obj.age.between(13, 14):
+      switch (true) {
+        case obj.distance >= 2700:
+          return 'Excellent';
+        case obj.distance.between(2400, 2699):
+          return 'Above Average';
+
+        case obj.distance.between(2200, 2399):
+          return 'Average';
+
+        case obj.distance.between(2100, 2199):
+          return 'Below Average';
+
+        case (obj.distance <= 2100):
+          return 'Poor';
+
+
+      }
+      break;
+
+    case obj.age.between(15, 16):
+      switch (true) {
+        case obj.distance >= 2800:
+          return 'Excellent';
+        case obj.distance.between(2500, 2799):
+          return 'Above Average';
+        case obj.distance.between(2300, 2499):
+          return 'Average';
+        case obj.distance.between(2200, 2299):
+          return 'Below Average';
+        case obj.distance <= 2200:
+          return 'Poor';
+      }
+      break;
+    case obj.age.between(17, 19):
+      switch (true) {
+        case obj.distance >= 3000:
+          return 'Excellent';
+        case obj.distance.between(2700, 2999):
+          return 'Above Average';
+        case obj.distance.between(2500, 2699):
+          return 'Average';
+        case obj.distance.between(2300, 2599):
+          return 'Below Average';
+        case obj.distance <= 2300:
+          return 'Poor';
+      }
+      break;
+
+    case obj.age.between(15, 16):
+      switch (true) {
+        case obj.distance >= 2800:
+          return 'Excellent';
+        case obj.distance.between(2500, 2799):
+          return 'Above Average';
+        case obj.distance.between(2300, 2499):
+          return 'Average';
+        case obj.distance.between(2200, 2299):
+          return 'Below Average';
+        case obj.distance <= 2200:
+          return 'Poor';
+      }
+      break;
+
+    case obj.age.between(20, 29):
+      switch (true) {
+        case obj.distance >= 2800:
+          return 'Excellent';
+        case obj.distance.between(2400, 2799):
+          return 'Above Average';
+        case obj.distance.between(2200, 2399):
+          return 'Average';
+        case obj.distance.between(1600, 2199):
+          return 'Below Average';
+        case obj.distance <= 1600:
+          return 'Poor';
+      }
+      break;
+
+    case obj.age.between(30, 39):
+      switch (true) {
+        case obj.distance >= 2700:
+          return 'Excellent';
+        case obj.distance.between(2300, 2699):
+          return 'Above Average';
+        case obj.distance.between(2000, 2299):
+          return 'Average';
+        case obj.distance.between(1500, 1999):
+          return 'Below Average';
+        case obj.distance <= 1500:
+          return 'Poor';
+      }
+      break;
+
+    case obj.age.between(40, 49):
+      switch (true) {
+        case obj.distance >= 2500:
+          return 'Excellent';
+        case obj.distance.between(2100, 2499):
+          return 'Above Average';
+        case obj.distance.between(1700, 2099):
+          return 'Average';
+        case obj.distance.between(1400, 1699):
+          return 'Below Average';
+        case obj.distance <= 1400:
+          return 'Poor';
+      }
+      break;
+
+    case (obj.age >= 50):
+      switch (true) {
+        case obj.distance >= 2400:
+          return 'Excellent';
+        case obj.distance.between(2000, 2399):
+          return 'Above Average';
+        case obj.distance.between(1600, 1999):
+          return 'Average';
+        case obj.distance.between(1300, 1599):
+          return 'Below Average';
+        case obj.distance <= 1300:
+          return 'Poor';
+      }
+      break;
+  }
+};
